32e74268dd451cd67859ee204b4d081faa52e2df
Deevanshu Kushwah <astroboiscosmos@gmail.com>
2025-01-22 16:21:52 +0530

    minor update

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Grid from "./grid/grid";
 function App() {
     return (
         <div className="w-screen h-screen bg-[#1f1f1f] flex justify-center items-center">
-            <Grid />
+            <Grid rows={15} cols={20} defaultColor="#000" />
         </div>
     );
 }
--- src/grid/grid.tsx
+++ src/grid/grid.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Cell from "./cell";
 
-const Grid: React.FC = () => {
-    const rows = 15; // Number of rows
-    const cols = 20; // Number of columns
-    const defaultColor = "#000";
+const Grid: React.FC<{ rows: number; cols: number; defaultColor: string }> = ({
+    rows,
+    cols,
+    defaultColor,
+}) => {
     const trailColors = [
         "rgb(83,248,0)",
         "rgb(40,212,0)",
@@ -17,53 +18,42 @@ const Grid: React.FC = () => {
     const [grid, setGrid] = useState<string[][]>(
         Array.from({ length: rows }, () => Array(cols).fill(defaultColor))
     );
-    const [currCol, setCurrCol] = useState<number>(19);
+    const [stripes, setStripes] = useState<number[]>([
+        cols - 6,
+        cols - 5,
+        cols - 4,
+        cols - 3,
+        cols - 2,
+        cols - 1,
+    ]);
 
     useEffect(() => {
-        let dir = -1; // Initial direction
-
+        const dir: number[] = [-1, -1, -1, -1, -1, -1];
         const t = setInterval(() => {
-            setCurrCol((prevCol) => {
-                const nextCol = prevCol + dir;
-                if (nextCol === cols) dir = -1; // Reverse at the right edge
-                if (nextCol === -1) dir = 1; // Reverse at the left edge
-                updateGrid(nextCol, dir); // Update the grid with trail effect
-                return nextCol;
+            setStripes((p) => {
+                return p.map((n, i) => {
+                    const next = n + dir[i];
+                    if (next === cols) dir[i] = -1;
+                    if (next === -1) dir[i] = 1;
+                    return next;
+                });
             });
-        }, 100); // 200 milliseconds
-
-        const updateGrid = (col: number, dir: number) => {
-            setGrid(() => {
-                // Initialize a new grid with the default color
-                let newGrid = Array.from({ length: rows }, () =>
-                    Array(cols).fill(defaultColor)
-                );
-
-                for (let k = 0; k < trailColors.length; k++) {
-                    // Calculate the column index for the trail
-                    const idx = col - k * dir;
-
-                    // Ensure the index stays within bounds
-                    if (idx >= 0 && idx < cols) {
-                        newGrid = newGrid.map((r) => {
-                            r[idx] = trailColors[k];
-                            return r;
-                        });
-                    }
-                }
-
-                return newGrid;
-            });
-        };
-
-        return () => clearInterval(t); // Cleanup on unmount
+        }, 100);
+        return () => clearInterval(t);
     }, []);
+
     return (
         <div className="grid grid-cols-20 gap-2 aspect-[20/15] h-[95vh]">
             {grid.map((row, rowIndex) =>
                 row.map((cell, colIndex) => (
                     <Cell
-                        color={cell}
+                        color={
+                            stripes.includes(colIndex)
+                                ? trailColors[
+                                      stripes.findIndex((p) => p === colIndex)
+                                  ]
+                                : "black"
+                        }
                         key={`${rowIndex}-${colIndex}`}
                         rowIndex={rowIndex}
                         colIndex={colIndex}
